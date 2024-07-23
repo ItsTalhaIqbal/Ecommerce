c27a4ae2ca8e9694ebcb6ebb0047d413a069bce5
hackerspidy1 <ti709888@gmail.com>
2024-07-23 16:07:31 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
+// next.config.js
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-}
-
-module.exports = nextConfig
+};
